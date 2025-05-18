3752eeb73645eb501a619e000a61ca9a168ab7d5
Priya2422 <priyapandey.2415@gmail.com>
2025-05-18 12:33:59 +0530

    added general route

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/products', require('./routes/products'));
